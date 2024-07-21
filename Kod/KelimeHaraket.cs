558b7d409729e565b2855a6875399749f92c3790
YavuzSelimAtes <yavuzslm60@gmail.com>
2024-07-21 21:35:16 +0300

    Harf Kutusunun hareketine ekleme yaptım ve text objesi ekledim

--- Kod/KelimeHaraket.cs
+++ Kod/KelimeHaraket.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class KelimeHaraket : MonoBehaviour
 {
+    public TextMeshProUGUI harf;
     GameObject oyunKontrol;
     GameObject yenikelime;
+    Vector3 pozisyon;
+    int kelimeKonum = 4;
+    int hiz = 3;
+
+    System.Random random;
     void Start()
     {
+        random = new System.Random();
         oyunKontrol = GameObject.Find("OyunKontrol");
         yenikelime = oyunKontrol.GetComponent<OyunKontrol>().klon;
+        harf.text = harfOlustur();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,6 +29,68 @@ public class KelimeHaraket : MonoBehaviour
             Destroy(yenikelime.GetComponent<KelimeHaraket>());
             yenikelime.GetComponent<Rigidbody2D>().gravityScale = 1;
             oyunKontrol.GetComponent<OyunKontrol>().KelimeOlustur();
+            kelimeKonum = 4;
         }
+    }
+
+    void kelimeHareket()
+    {
+        transform.Translate(Vector2.down * hiz * Time.deltaTime);
+    }
+    void haraket()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (kelimeKonum == 1)
+            {
+                return;
+            }
+            else
+            {
+                pozisyon.x -= 3.25f;
+                transform.position = pozisyon;
+            }
+
+            kelimeKonum--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            if (kelimeKonum == 8)
+            {
+                return;
+            }
+            else
+            {
+                pozisyon.x += 3.25f;
+                transform.position = pozisyon;
+            }
+
+            kelimeKonum++;
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+            hiz = 12;
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+            hiz = 3;
+        
+    }
+
+    public string harfOlustur()
+    {
+        char harfUret;
+        do
+        {
+            harfUret = (char)random.Next('A', 'Z' + 1);
+        } while (harfUret == 'Q' || harfUret == 'W' || harfUret == 'X');
+
+        return harfUret.ToString();
+    }
+    void Update()
+    {
+        pozisyon = transform.position;
+        kelimeHareket();
+        haraket();
     }
 }
--- Kod/OyunKontrol.cs
+++ Kod/OyunKontrol.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,62 +10,21 @@ public class OyunKontrol : MonoBehaviour
     public GameObject kelime;
     public GameObject klon;
     public Transform kelimeBaslangic;
-    Vector3 pozisyon;
-    int kelimeKonum = 4;
+    TextMeshProUGUI harf;
+
     void Start()
     {
-        klon=Instantiate(kelime, kelimeBaslangic.position, kelime.transform.rotation);
-        pozisyon = klon.transform.position;
+        klon = Instantiate(kelime, kelimeBaslangic.position, kelime.transform.rotation);
+
     }
 
     public void KelimeOlustur()
     {
-        kelimeKonum = 4;
         klon = Instantiate(kelime, kelimeBaslangic.position, kelime.transform.rotation);
     }
 
-    void kelimeHareket()
-    {
-        klon.transform.Translate(Vector2.down * 3 * Time.deltaTime);
-    }
-    void haraket()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (kelimeKonum == 1)
-            {
-                return;
-            }
-            else
-            {
-                pozisyon.x -= 3.25f;
-                klon.transform.position = pozisyon;
-            }
 
-            kelimeKonum--;
-        }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-
-            if (kelimeKonum == 8)
-            {
-                return;
-            }
-            else
-            {
-                pozisyon.x += 3.25f;
-                klon.transform.position = pozisyon;
-            }
-
-            kelimeKonum++;
-        }
-    }
-    void Update()
-    {
-        pozisyon = klon.transform.position;
-        kelimeHareket();
-        haraket();
-    }
+   
 
 }
